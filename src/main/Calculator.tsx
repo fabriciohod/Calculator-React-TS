cd83273799662eac8ee1f2c7a7789777cddd4913
fabricio henrique <fabriciohod@hotmail.com>
2020-07-17 14:26:28 -0300

    _

---
+++ src/main/Calculator.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Calculator.css'
+
+export class Calculator extends Component {
+    render() {
+        return (
+            <div className="calculator">
+                
+            </div>
+        )
+    }
+}
+
+export default Calculator
